bccca27b7cf1304771003191ddaabe1f834312f4
shusukeO <56382189+shusukeO@users.noreply.github.com>
2025-06-28 19:55:00 +0900

    update nedb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const Datastore = require('nedb');
+const Datastore = require('@seald-io/nedb');
 const path = require('path');
 
 const app = express();
