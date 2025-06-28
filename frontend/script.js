2fcc789200ac38c47cb643e993a959d276ec4eae
shusukeO <56382189+shusukeO@users.noreply.github.com>
2025-06-28 22:53:23 +0900

    u

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 // バックエンドのAPIエンドポイント
 const API_BASE_URL =
-  window.location.hostname === "onrender"
+  window.location.hostname === "onrender.com"
     ? "https://vibe-chat-backend-3699.onrender.com/api"
     : "http://localhost:3000/api"; // ←Renderの実際のURLに変更
 
